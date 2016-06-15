d9be7410860a534f0398136a395cb49124f68b7a
SandraBollo <sandra.bollo.b@gmail.com>
2016-06-15 09:22:15 -0500

    :( soy la mas trite del mundo mundial prometo arreglarlo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,72 @@
 (function(){
-	//variables//
-	var lista= document.getElementById("lista"),
-	tarreaImput=document.getElementById("tarreaImput"),
-	
+    // Variables
+    var lista = document.getElementById("lista"),
+        tareaInput = document.getElementById("tareaInput"),
+        btnNuevaTarea = document.getElementById("btn-agregar");
+ 
+    // Funciones
+    var agregarTarea = function(){
+        var tarea = tareaInput.value,
+            nuevaTarea = document.createElement("li"),
+            enlace = document.createElement("a"),
+            contenido = document.createTextNode(tarea);
+ 
+        if (tarea === "") {
+            tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
+            tareaInput.className = "error";
+            return false;
+        }
+ 
+       // Agregamos el contenido al enlace
+        enlace.appendChild(contenido);
+        // Le establecemos un atributo href
+        // Agrergamos el enlace (a) a la nueva tarea (li)
+        nuevaTarea.appendChild(enlace);
+        // enlace.insertAdjacentHTML('afterend','<i class="fa fa-trash fa-1x" aria-hidden="true"></i>');
+        // Agregamos la nueva tarea a la lista
+        lista.appendChild(nuevaTarea); 
+        for (var i = 0; i <= lista.children.length -1; i++) {
+            lista.children[i].addEventListener("click", function(){
+                this.parentNode.removeChild(this);
+            });
+        }
+ 
+    };
+    var comprobarInput = function(){
+        tarea.className = "";
+        tearea.setAttribute("placeholder", "Agrega tu tarea");
+    };
+ 
+    var eleminarTareas = function(){
+        this.parentNode.removeChild(this);
+    };
+ 
+    // Eventos
+ 
+    // Agregar Tarea
+    btnNuevaTarea.addEventListener("click", agregarTarea);
+ 
+    // Comprobar Input
+    tarea.addEventListener("click", comprobartarea);
+ 
+    // Borrando Elementos de la lista
+    for (var i = 0; i <= lista.children.length -1; i++) {
+        lista.children[i].addEventListener("click", eleminarTarea);
+    }
 }());
+ 
+
+function checkWinner(element) {
+    var result = true;
+    var elementId = parseInt(element.attr("id"));
+    $.each(function(index,evalutionRoute) {
+        if (evalutionRoute.id === elementId) {
+            $.each(evalutionRoute.winningPath, function(index, path) {
+                $.each(path,function(index, elem) {
+                    result = result && $("#"+elem).val() === element.val();
+                });   
+            });
+        }
+    });
+    return result;
+}         
